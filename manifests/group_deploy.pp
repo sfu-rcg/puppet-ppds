75cf8b1d175f45f8705a4bf92f79d4f773efbb00
asasfu <asa188@sfu.ca>
2016-09-07 16:26:33 -0700

    Didn't need a group_deploy String, use

--- manifests/group_deploy.pp
+++ manifests/group_deploy.pp
@@ -1,18 +1,18 @@
+# Utilize the $title of this define type to deploy groups of ppds to clients, such as ppds::group_deploy { 'KonicaMinolta': }  and it will deploy the folder recursively to clients
 define ppds::group_deploy(
-  String[1] $group_name => '',
 ) {
   include ppds
 
-  file { "/opt/ppd/${group_name}":
+  file { "/opt/ppd/${title}":
     owner   => 'root',
     group   => 'root',
     recurse => true,
     ensure  => directory,
-    source  => "puppet:///modules/ppds/${group_name}"
+    source  => "puppet:///modules/ppds/${title}"
   }
 
-  file { "/usr/share/ppd/${group_name}":
+  file { "/usr/share/ppd/${title}":
     ensure => link,
-    target => "/opt/ppd/${group_name}",
+    target => "/opt/ppd/${title}",
   }
 }
